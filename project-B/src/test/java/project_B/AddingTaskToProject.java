35f84ae44f9569fc4b403a2057e2328d73f97e39
Louis-2024 <weiheng.xiao@mail.mcgill.ca>
2023-11-02 16:17:18 -0400

    Updated with step definitions

--- project-B/src/test/java/project_B/AddingTaskToProject.java
+++ project-B/src/test/java/project_B/AddingTaskToProject.java
@@ -6,7 +6,58 @@ import io.cucumber.java.en.When;
 
 public class AddingTaskToProject {
 
-
-
+    @Given("the todo with todo_id: {int}, todo_title: {string}, todo_doneStatus: false, todo_description: {string}, exists")
+    public void the_todo_with_todo_id_todo_title_todo_done_status_false_todo_description_exists(Integer int1, String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+       throw new io.cucumber.java.PendingException();
+    }
+     
+    @Given("project with project_title: {string}, project_completed: false, project_active: false, project_description: {string}, project_tasks: [], exists")
+    public void project_with_project_title_project_completed_false_project_active_false_project_description_project_tasks_exists(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @When("the user add the todo to the project")
+    public void the_user_add_the_todo_to_the_project() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @Then("the todo should exist in the project's project_tasks field")
+    public void the_todo_should_exist_in_the_project_s_project_tasks_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @Given("project with project_title: {string}, project_completed: false, project_active: false, project_description: {string}, project_tasks: [\\{id:{int}},\\{id:{int}}], exists")
+    public void project_with_project_title_project_completed_false_project_active_false_project_description_project_tasks_exists(String string, String string2, Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @Then("nothing will change")
+    public void nothing_will_change() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @Given("the todo with todo_id: {int}, todo_title: {string}, todo_doneStatus: false, todo_description: {string}, does not exist")
+    public void the_todo_with_todo_id_todo_title_todo_done_status_false_todo_description_does_not_exist(Integer int1, String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @Then("the todo should not exist in the project's project_tasks field")
+    public void the_todo_should_not_exist_in_the_project_s_project_tasks_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+     
+    @Then("an error message should be displayed")
+    public void an_error_message_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
--- project-B/src/test/java/project_B/CreatingProjectWithFields.java
+++ project-B/src/test/java/project_B/CreatingProjectWithFields.java
@@ -1,5 +1,49 @@
 package project_B;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class CreatingProjectWithFields {
+    @Given("the todo with todo_id: {int}, todo_title: {string}, todo_doneStatus: false, todo_description: {string}, exists")
+    public void the_todo_with_todo_id_todo_title_todo_done_status_false_todo_description_exists(Integer int1, String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
     
+    @When("the user creates project with project_title: {string}, project_completed: false, project_active: false, project_description: {string}, project_tasks: []")
+    public void the_user_creates_project_with_project_title_project_completed_false_project_active_false_project_description_project_tasks(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @Then("the new project with specific fields should exist under Projects")
+    public void the_new_project_with_specific_fields_should_exist_under_projects() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @When("the user creates project with project_title: {string}, project_completed: false, project_active: false, project_description: {string}, project_tasks: [\\{id:{int}}]")
+    public void the_user_creates_project_with_project_title_project_completed_false_project_active_false_project_description_project_tasks(String string, String string2, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @Given("the todo with todo_id: {int}, does not exist")
+    public void the_todo_with_todo_id_does_not_exist(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @Then("the new project with specific fields should not exist under Projects")
+    public void the_new_project_with_specific_fields_should_not_exist_under_projects() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @Then("an error message should be displayed")
+    public void an_error_message_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
