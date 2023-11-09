5088600c360bb8cf72298884f4e92986d551a62b
Louis896 <weiheng.xiao@mail.mcgill.ca>
2023-11-08 21:41:16 -0500

    some progress

---
+++ project-B/src/test/java/project_B/tests/CreatingProjectWithFields.java
@@ -0,0 +1,86 @@
+package project_B;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+
+import static org.junit.Assert.*;
+import java.util.List;
+import project_B.BaseStepDefinition;
+
+
+public class CreatingProjectWithFields extends BaseStepDefinition {
+
+    @Given("^the todo exists$")
+    public void the_todo_exists(DataTable datatable) {
+        List<List<String>> rows = datatable.asLists(String.class);
+        List<String> task_1=rows.get(1);
+        JSONObject response = Unirest.get("/todos/1").asJson().getBody().getObject();
+        JSONObject task_1_response=response.getJSONArray("todos").getJSONObject(0);
+        assertNotNull(task_1_response);
+        assertEquals(task_1_response.getString("title"), task_1.get(1));
+        assertEquals(task_1_response.getString("doneStatus"), task_1.get(2).toString());
+        assertEquals(task_1_response.getString("description"), (task_1.get(3) != null) ? task_1.get(3):"");
+    }
+    
+    @When("^the user creates project without tasks (.*) (true|false) (true|false) (.*)$")
+    public void the_user_creates_project_without_tasks(String project_title, Boolean project_completed, Boolean project_active, String project_description) {
+        Unirest.post("/projects")
+        .body("{\n\"title\":\"" + project_title + "\",\n\"description\":\"" + project_description + "\",\n\"active\":" + project_active + ",\n\"completed\":" + project_completed + "\n}")
+        .asJson();
+        JSONObject response=findProjectByName(project_title);
+        assertNotNull(response);
+    }
+
+    @When("^the user creates project with task (.*) (true|false) (true|false) (.*) (.*)$")
+    public void the_user_creates_project_with_task(String project_title, Boolean project_completed, Boolean project_active, String project_description, String project_tasks) {
+        Unirest.post("/projects")
+        .body("{\n\"title\":\"" + project_title + "\",\n\"description\":\"" + project_description + "\",\n\"active\":" + project_active + ",\n\"completed\":" + project_completed + "\n}")
+        .asJson();
+        assertTrue(true);
+    }
+
+    @When("^the user creates project with non-existing task (.*) (true|false) (true|false) (.*) (.*)$")
+    public void the_user_creates_project_with_non_existing_task(String project_title, Boolean project_completed, Boolean project_active, String project_description, String project_tasks) {
+        Unirest.post("/projects")
+        .body("{\n\"title\":\"" + project_title + "\",\n\"description\":\"" + project_description + "\",\n\"active\":" + project_active + ",\n\"completed\":" + project_completed + "\n}")
+        .asJson();
+        assertTrue(true);
+    }
+    
+    @Then("^the new project with specific fields (.*) (true|false) (true|false) (.*) should exist under Projects$")
+    public void the_new_project_with_specific_fields_should_exist_under_projects(String project_title, Boolean project_completed, Boolean project_active, String project_description) {
+        JSONObject response=findProjectByName(project_title);
+        assertNotNull(response);
+        assertEquals(project_completed+"", response.getString("completed"));
+        assertEquals(project_active+"", response.getString("active"));
+        assertEquals(project_description, response.getString("description"));
+        assertFalse(response.has("tasks"));
+    }
+
+    @Then("^the new project with specific fields and tasks should exist under Projects$")
+    public void the_new_project_with_specific_fields_and_tasks_should_exist_under_projects() {
+        assertTrue(true);
+    }
+
+    
+    @Given("^the todo with todo_id 3 does not exist$")
+    public void the_todo_with_todo_id_does_not_exist(DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        assertTrue(true);
+    }
+    
+    @Then("^the new project with specific fields should not exist under Projects$")
+    @And ("^an error message should be displayed$")
+    public void the_new_project_with_specific_fields_should_not_exist_under_projects() {
+        // Write code here that turns the phrase above into concrete actions
+        assertTrue(true);
+    }
+
+}
---
+++ project-B/src/test/resources/project_B/CreatingProjectWithFields.feature
@@ -0,0 +1,38 @@
+@CreatingProjectWithFields
+Feature: Creating Project with specific fields
+
+    As a user, I want to create a project with specific fields, so I can prepare the project for task organization
+
+    #Normal flow
+    Scenario Outline: Creating project with specific fields and no relationship to task
+        Given the todo exists
+        | id    | title             | doneStatus | description  |
+        | 1     | scan paperwork    | false      |              |
+        When the user creates project without tasks <project_title1> <project_completed1> <project_active1> <project_description1>
+        Then the new project with specific fields <project_title1> <project_completed1> <project_active1> <project_description1> should exist under Projects 
+        Examples:
+            | project_title1| project_completed1 | project_active1 | project_description1 | project_tasks1  |
+            | ECSE429       | false              | false           |                      |[]               |
+
+    #Alternate flow 
+    Scenario Outline: Creating project with specific fields and with relationship to task 
+        Given the todo exists 
+        | id | title          | doneStatus | description | 
+        | 1  | scan paperwork | false      |             | 
+        When the user creates project with task <project_title2> <project_completed2> <project_active2> <project_description2> <project_tasks2> 
+        Then the new project with specific fields and tasks should exist under Projects 
+        Examples: 
+            | project_title2 | project_completed2 | project_active2 | project_description2 | project_tasks2 | 
+            | ECSE429        | false              | false           |                      |[{"id": 1}]     |
+    
+    #Error flow
+    Scenario Outline: Creating project with specific fields and with relationship to a non-existing task
+        Given the todo with todo_id 3 does not exist
+        | id    | title             | doneStatus | description  |
+        | 1     | scan paperwork    | false      |              |
+        When the user creates project with non-existing task <project_title3> <project_completed3> <project_active3> <project_description3> <project_tasks3>
+        Then the new project with specific fields should not exist under Projects
+        And an error message should be displayed
+        Examples:
+            | project_title3 | project_completed3 | project_active3 | project_description3 | project_tasks3  |
+            | ECSE429        | false              | false           |                      |[{"id": 3}]      |
